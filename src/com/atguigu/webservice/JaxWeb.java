032c14308325e8173e21862cd706717644d1344e
luvutaeyeon <luvtaeyeon@outlook.com>
2017-02-28 10:49:46 +0800

    luvtaeyeon

---
+++ src/com/atguigu/webservice/JaxWeb.java
@@ -0,0 +1,16 @@
+package com.atguigu.webservice;
+
+public class JaxWeb {
+
+	interface WW{
+		
+		int fn(int a,int b);
+	}
+	
+	public static void main(String[] args) {
+		WW ww = (int a,int b)->a-b;
+		
+		System.out.println(ww.fn(8, 9));
+		
+	}
+}
